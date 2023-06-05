aafb44a15d1bbaef1d995ba5623457d560a9fb80
nknikl2 <nikltotatarin@gmail.com>
2023-06-05 22:51:23 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import io
+import json
+from PIL import Image
+from fastapi import File, FastAPI
+import torch
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+img = 'https://im.wampi.ru/2023/06/05/123.png'
+results = model(img)
+print(results.pandas().xyxy[0].to_json(orient="records"))
+
+app = FastAPI()
+
+@app.post("/objectdetection/")
+async def get_body(file: bytes = File(...)):
+    input_image = Image.open(io.BytesIO(file)).convert("RGB")
+    results = model(input_image)
+    results_json = json.loads(results.pandas().xyxy[0].to_json(orient="records"))
+    return {"result": results_json}
